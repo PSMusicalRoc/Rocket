c3aefb3af7457f86980f8afb6eb578d5dee2bf4d
Roc <psmusicalroc@gmail.com>
2023-10-12 21:09:05 -0400

    Restructured files so that we only need STB defines and the main RocketEngine.hpp include

---
+++ include/Base/Keyboard.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <GLFW/glfw3.h>
+
+#include "Application.hpp"
+
+enum class RocketKey {
+    ENTER = 1,
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+    I,
+    J,
+    K,
+    L,
+    M,
+    N,
+    O,
+    P,
+    Q,
+    R,
+    S,
+    T,
+    U,
+    V,
+    W,
+    X,
+    Y,
+    Z,
+    ESC,
+    BACKSPACE,
+    SPACE
+};
---
+++ include/RocketEngine.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+// Needed backends
+
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
+// Base features
+
+#include "Base/Application.hpp"
+#include "Base/Keyboard.hpp"
+
+// ECS
+
+#include "Roc_ECS.h"
+
+// OpenGL Additions
+
+#include "Roc_GL/CoordinateSystem.hpp"
+#include "Roc_GL/Shader.hpp"
+#include "Roc_GL/Texture.hpp"
+
+// RScene Language
+
+#include "RScene/LoadRscene.hpp"
+
+// Logger
+
+#include "RocLogger/RocLogger.hpp"
